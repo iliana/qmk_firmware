a5265f4d99f5b2c636f0e6020699a22d612f4fc8
Iliana Weller <ilianaw@buttslol.net>
2017-02-15 23:25:51 -0800

    Revert Ctrl/Esc mod-tap as it gives me issues

--- keyboards/planck/keymaps/ilianaw/keymap.c
+++ keyboards/planck/keymaps/ilianaw/keymap.c
@@ -26,7 +26,6 @@ enum keycodes {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define EMOJI MO(_EMOJI)
-#define CTLESC CTL_T(KC_ESC)
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -55,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {CTLESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
   {KC_LCTL, KC_LGUI, KC_LALT, EMOJI  , LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
